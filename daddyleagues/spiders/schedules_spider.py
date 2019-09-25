1fe0b84572e5b04d2cab83da200f0acfa4cafa58
Konstantin <kpletenev@yandex.ru>
2019-09-25 21:58:42 +0300

    update links

--- daddyleagues/spiders/schedules_spider.py
+++ daddyleagues/spiders/schedules_spider.py
@@ -10,8 +10,8 @@ class SchedulesSpider(scrapy.Spider):
     def start_requests(self):
         # import pdb; pdb.set_trace()
         self.conn = sqlite3.connect('daddyleagues.db')
-        url = 'http://old.daddyleagues.com/uflrus/schedules'
-        #url = 'http://www.daddyleagues.com/uflrus/schedules'
+        #url = 'http://old.daddyleagues.com/uflrus/schedules'
+        url = 'http://www.daddyleagues.com/uflrus/schedules'
 
         c = self.conn.cursor()
         #debug
@@ -33,7 +33,9 @@ class SchedulesSpider(scrapy.Spider):
         #weeks = [(1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0), (9, 0), (10, 0), (11, 0), (12, 0), (13, 0), (14, 0), (15, 0), (16, 0), (17, 0), (18, 0), (19, 0), (21, 0)]
         weeks = [(int(w), 0)
                  for w
-                 in response.css('div.weekSelector li a::attr(rel)').extract()]
+                 #in response.css('div.weekSelector li a::attr(rel)').extract()]
+                 in response.css('.tab-pane.fadeshow.active div a::text').extract()]
+
 
         c = self.conn.cursor()
         c.executemany('insert into week values (null, ?, ?)', weeks)
@@ -46,14 +48,14 @@ class SchedulesSpider(scrapy.Spider):
     def parse_week(self, response):
         # import pdb; pdb.set_trace()
         week = response.meta['week']
-        li = response.css('li')
+        games = response.css('div.card.b.mb-2')
         c = self.conn.cursor()
         #debug
         #c.execute('drop table games')
         c.execute('CREATE TABLE IF NOT EXISTS games (week INTEGER, team1_id, score1, score2, team2_id, vs, sended)')
         count = c.execute('select count(*) from games where week = ?',
                           (week,)).fetchone()
-        if count[0] == len(li):
+        if count[0] == len(games):
             # import pdb; pdb.set_trace()
             c.execute('update week set ended = 1 where week = ?', (week,))
             self.conn.commit()
@@ -68,22 +70,29 @@ select week from week where ended = 0 order by id limit 1
             else:
                 yield dict()
         else:
-            for s in li:
+            for s in games:
                 # Возможно потом добавить lower для названий
                 # import pdb; pdb.set_trace()
-                names = s.css('div.name::text').extract()
-                scores = s.css('span.score > span::text').extract()
-                score1 = int(scores[0].strip())
-                score2 = int(scores[1].strip())
+               # names = s.css('div.name::text').extract()
+               # scores = s.css('span.score > span::text').extract()
+               # score1 = int(scores[0].strip())
+               # score2 = int(scores[1].strip())
+               # vs = s.css('a::attr(href)').extract()[0]
+               # vs = vs.replace("old", "www", 3)
+
+                name1 = s.css('.col-2 div.h2.m-0.text-bold::text').extract()
+                name2 = s.css('.col-3 div.h2.m-0.text-bold::text').extract()
+                scores = s.css('.col-6 div h4.mt-3 a::text').extract()
+                scores = scores[0].strip().split('vs')
                 vs = s.css('a::attr(href)').extract()[0]
-                vs = vs.replace("old", "www", 3)
-                if score1 != 0 or score2 != 0:
+
+                if int(scores[0].strip()) != 0 or int(scores[1].strip()) != 0:
                     yield {
                         'week': week,
-                        'team1': names[0].strip(),
-                        'score1': score1,
-                        'team2': names[1].strip(),
-                        'score2': score2,
+                        'team1': name1[0].strip(),
+                        'score1':  int(scores[0].strip()),
+                        'team2': name2[0].strip(),
+                        'score2': int(scores[1].strip()),
                         'vs': vs
                     }
 
